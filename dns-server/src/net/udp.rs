cd380149092c8e36d63a7be4f698246acce2e9c0
Austin Schaaf <austin.schaaf@gmail.com>
2026-03-02 09:17:29 -0600

    Upstreaming DNS

--- dns-server/src/net/udp.rs
+++ dns-server/src/net/udp.rs
@@ -1,6 +1,5 @@
 use std::net::UdpSocket;
 use std::io;
-
 use crate::dns::header::DnsHeader;
 use crate::dns::question::DnsQuestion;
 
@@ -20,15 +19,28 @@ pub fn run() -> io::Result<()> {
         println!("Echoed packet back {}\n", src);
 
         let header = DnsHeader::parse(&buffer);        
-        println!("Questions: {}", header.qdcount);
-
-        let mut pos = 12; //DNS Header is 12 bytes
+        let mut pos = 12;
 
         if header.qdcount > 0 {
             let (question, _) = DnsQuestion::parse(&buffer, pos);
-
             println!("Domain: {}", question.name);
-            println!("Type: {}", question.qtype);
         }
+
+        //Forward request upstream
+        let response = forward_to_upstream(&buffer[..size])?;
+
+        //Send response back to original client
+        socket.send_to(&response, src)?;
     }
+}
+
+fn forward_to_upstream(request: &[u8]) -> io::Result<(Vec<u8>)> {
+    let upstream = "8.8.8.8:53";
+    let upstream_socket = UdpSocket::bind("0.0.0.0:0")?;
+
+    upstream_socket.send_to(request, upstream)?;
+
+    let mut response_buffer = [0u8; 512];
+    let (size, _) = upstream_socket.recv_from(&mut response_buffer)?;
+    Ok(response_buffer[..size].to_vec())
 }
